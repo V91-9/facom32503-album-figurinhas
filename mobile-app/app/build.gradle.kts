86ae04efff76457d44950f47ce01fffd4becfa69
V91-9 <vinicius2199@gmail.com>
2026-07-10 11:11:02 -0300

    fix: reordena plugin do Hilt para carregar apos o plugin do Android

--- mobile-app/app/build.gradle.kts
+++ mobile-app/app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-
-    id("com.google.devtools.ksp")
+    
     id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
