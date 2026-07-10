e3d6efe79e5406a3acc8e804d49719db7c23692d
V91-9 <vinicius2199@gmail.com>
2026-07-10 11:30:12 -0300

    fix: adiciona kotlin.android e ajusta ordem estrita dos plugins para o Hilt

--- mobile-app/app/build.gradle.kts
+++ mobile-app/app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    
+
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
     id("org.jetbrains.kotlin.plugin.serialization")
